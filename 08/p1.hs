47536ed9835b57a9aeafcf052622433cba360d91
Patrick Wells <patrick.wells.95@gmail.com>
2024-12-08 10:10:58 -0600

    Day 8 solutions

---
+++ 08/p1.hs
@@ -0,0 +1,17 @@
+import System.IO
+import Common
+import Data.Set (toList)
+import Data.List (nub)
+
+main :: IO()
+main = do
+    handle <- openFile "input.txt" ReadMode
+    contents <- hGetContents handle
+    let ls = lines contents 
+    let size = (length ls, length (ls !! 0))
+    let antennas = parseLines ls
+    let frequencies = freqs antennas
+    let as = map (findFreq antennas) (toList frequencies)
+    let antinodes = filter (inGrid size) (concat (map (getAntiNodes antiNodes) as))
+    print $ length antinodes
+    print $ length $ nub antinodes
