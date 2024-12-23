1933b5b37c71f547bd12c018723a3e67ea0b3403
Patrick Wells <patrick.wells.95@gmail.com>
2024-12-23 12:48:53 -0800

    Small updates

--- 10/10.hs
+++ 10/10.hs
@@ -1,7 +1,7 @@
+import Data.List
+import Data.Maybe
 import Matrix
 import System.IO
-import Data.Maybe
-import Data.List
 
 getScores :: [(Index, [Index])] -> [Int]
 getScores = map (\(_, p) -> length p)
@@ -12,25 +12,25 @@ findUniqueTrailEnds m = map (\(s, es) -> (s, nub es)) (findTrailEnds m)
 findTrailEnds :: Matrix Int -> [(Index, [Index])]
 findTrailEnds m = map (\s -> (s, findPeak m 0 s)) (findAll m 0)
 
-
 findPeak :: Matrix Int -> Int -> Index -> [Index]
-findPeak mat level idx 
-    | isNothing entry = []
-    | level /= val = [] 
-    | val == 9 = [idx]
-    | otherwise = foldl (++) [] (map (findPeak mat (level+1)) newIdxs) where
+findPeak mat level idx
+  | isNothing entry = []
+  | level /= val = []
+  | val == 9 = [idx]
+  | otherwise = foldl (++) [] (map (findPeak mat (level + 1)) newIdxs)
+  where
     entry = get mat idx
     val = fromJust entry
-    newIdxs = map (\d -> idx ^+ toIndexShift (toEnum d::Direction)) [0..3]
+    newIdxs = map (\d -> idx ^+ toIndexShift (toEnum d :: Direction)) [0 .. 3]
 
-main :: IO()
+main :: IO ()
 main = do
-    handle <- openFile "input.txt" ReadMode
-    contents <- hGetContents handle
-    let m = readDigits $ lines contents
-    let p1peaks = findUniqueTrailEnds m
-    let p1score = sum $ getScores p1peaks
-    print $ "Part 1: " ++ show p1score
-    let p2peaks = findTrailEnds m
-    let p2score = sum $ getScores p2peaks
-    print $ "Part 2: " ++ show p2score
+  handle <- openFile "input.txt" ReadMode
+  contents <- hGetContents handle
+  let m = readDigits $ lines contents
+  let p1peaks = findUniqueTrailEnds m
+  let p1score = sum $ getScores p1peaks
+  print $ "Part 1: " ++ show p1score
+  let p2peaks = findTrailEnds m
+  let p2score = sum $ getScores p2peaks
+  print $ "Part 2: " ++ show p2score
